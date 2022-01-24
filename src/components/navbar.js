1bc5efe29a585787b612d7214db7822bd3d2042c
Santi <santi.legui@hotmail.com>
2022-01-24 20:01:36 -0300

    agregue NavBar

---
+++ src/components/navbar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return(
+        <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+            <h1 class="navbar-brand" href="#">Home</h1>
+            <h4>Productos</h4>
+            <h4>Contacto</h4>
+            <h4>Nosotros</h4>
+        </div>
+        </nav>
+    );
+};
+
+export default NavBar;
